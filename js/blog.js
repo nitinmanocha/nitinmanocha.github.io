16f74c422d9ebd8063a2d7dd18e165b650e4b32d
nitinatqdesq <nitin@qdesq.com>
2020-02-20 15:30:50 +0530

    new website

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,10 @@
 fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@nitinmanocha16')
    .then((res) => res.json())
    .then((data) => {
-      // Filter for acctual posts. Comments don't have categories, therefore can filter for items with categories bigger than 0
-      const res = data.items //This is an array with the content. No feed, no info about author etc..
-      const posts = res.filter(item => item.categories.length > 0) // That's the main trick* !
+      
+      const res = data.items 
+      const posts = res.filter(item => item.categories.length > 0) 
 
-      // Functions to create a short text out of whole blog's content
       function toText(node) {
          let tag = document.createElement('div')
          tag.innerHTML = node
@@ -18,26 +17,30 @@ fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@nit
          text
       }
 
-      // Put things in right spots of markup
+
       let output = '';
       posts.forEach((item) => {
+          var categories = ''
+          item.categories.forEach((e)=>{
+            categories = categories + " • " + e
+          })
          output += `
-          <div class="aboutItem col-md-6 col-sm-6 col-xs-12 clearfix" data-scroll-reveal="enter right move">
-                        <div class="aboutText">
-							• <h4>${shortenText(item.title, 0, 50)+ '...'} </h4>
-							<div>
-								<img src="${item.thumbnail}"  style="width:300px; height:150px;"></img>
-							</div>
-							
-										<h6><i class="fa fa-user fa-2x black"></i>  ${item.author} &nbsp &nbsp
-										<i class="fa fa-clock-o fa-2x black"></i>  ${shortenText(item.pubDate,0 ,10)}</a></h6>
-							
-								<div class="aboutSeparator"></div>
-								  <b><p class="blog__intro bold">${'...' + shortenText(toText(item.content),60, 300)+ '...'}</p></b>
-								<a href="${item.link}" class="btn btn-default btn-black">Read More</a>
-							</div>
-						</div>
-					</div><br><br>
+                    <div class="blog-entry ftco-animate d-md-flex">
+                    <a href="${item.link}" class="img img-2" style="background-image: url(${item.thumbnail});"></a>
+                    <div class="text text-2 p-4">
+          <h3 class="mb-2"><a href="">${shortenText(item.title, 0, 50)+ '...'}</a></h3>
+          <div class="meta-wrap">
+                            <p class="meta">
+                  <span>${shortenText(item.pubDate,0 ,10)}</span>
+                  <span><a href="${item.link}">${categories}</a></span>
+                  <span> ${item.author}</span>
+                  <span> </span>
+              </p>
+          </div>
+          <p class="mb-4">${'...' + shortenText(toText(item.content),0, 300)+ '...'}</p>
+          <p><a href="${item.link}" class="btn-custom">Read More <span class="ion-ios-arrow-forward"></span></a></p>
+        </div>
+                </div>
 		 ` 
 		 
 
